7f2918d54f5d35953f0a0768305888e370ff72ff
sameor <sameor@macbookpro.local>
2019-05-29 17:30:49 +0800

    add rand string

---
+++ str.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	letterIdxBits = 6
+	letterIdxMask = 1<<letterIdxBits - 1
+	letterIdxMax  = 63 / letterIdxBits
+)
+
+func RandString(n int) string {
+	buffer := make([]byte, n)
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			buffer[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(buffer)
+}
+
+
